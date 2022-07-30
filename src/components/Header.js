22eafa90f8dd7361060bdb67ae61986ec39ff163
Squashim <konrad18v1@wp.pl>
2022-07-30 15:21:59 +0200

    17% done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.scss";
 
 import logo from "../images/logo.svg";
@@ -7,31 +7,38 @@ import cart from "../images/icon-cart.svg";
 import avatar from "../images/image-avatar.png";
 
 const Header = () => {
+	const [menuOpen, setMenuOpen] = useState(false);
+	const menuToggler = () => setMenuOpen((p) => !p);
 	return (
 		<div className={styles.header}>
 			<div className={styles.header__content}>
-				<div className={styles.header__content__nav}>
-					<img src={menu} alt="" id={styles.menuIcon}></img>
+				<div className={styles.header__content__left}>
+					<img
+						src={menu}
+						alt=""
+						id={styles.menuIcon}
+						onClick={menuToggler}
+					></img>
 					<img src={logo} alt="sneakers"></img>
-					<ul className={styles.navList}>
-						<li>
-							<a href="#">Collections</a>
-						</li>
-						<li>
-							<a href="#">Men</a>
-						</li>
-						<li>
-							<a href="#">Women</a>
-						</li>
-						<li>
-							<a href="#">About</a>
-						</li>
-						<li>
-							<a href="#">Contact</a>
-						</li>
-					</ul>
+					<nav className={`styles[nav--open]`}>
+						<a className={styles.nav__item} href="#">
+							Collections
+						</a>
+						<a className={styles.nav__item} href="#">
+							Men
+						</a>
+						<a className={styles.nav__item} href="#">
+							Women
+						</a>
+						<a className={styles.nav__item} href="#">
+							About
+						</a>
+						<a className={styles.nav__item} href="#">
+							Contact
+						</a>
+					</nav>
 				</div>
-				<div className={styles.header__content__user}>
+				<div className={styles.header__content__right}>
 					<img src={cart} alt="" id={styles.cart}></img>
 					<img src={avatar} alt="" id={styles.profile}></img>
 				</div>
